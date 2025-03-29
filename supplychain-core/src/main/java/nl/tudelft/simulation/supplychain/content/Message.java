c1452ab527ede2f224f4d5e7d7778d2fe732b35e
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-03-29 12:16:23 +0100

    Update content -> message

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/content/Message.java
@@ -0,0 +1,28 @@
+package nl.tudelft.simulation.supplychain.content;
+
+import nl.tudelft.simulation.supplychain.actor.Actor;
+
+/**
+ * A message, which can be sent from a sender to a receiver. Extend this class to add further fields.
+ * <p>
+ * Copyright (c) 2022-2023 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public abstract class Message extends Content
+{
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Construct a new message.
+     * @param sender Actor; the sender (necessary for a possible reply)
+     * @param receiver Actor; the receiver
+     */
+    public Message(final Actor sender, final Actor receiver)
+    {
+        super(sender, receiver);
+    }
+
+}
