8a182c9b8de9be455852c7d1cfbc10bfaacbf537
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-03-31 21:20:19 +0200

    Make AutonomousProcess an abstract class

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/process/AutonomousProcess.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/process/AutonomousProcess.java
@@ -1,6 +1,8 @@
 package nl.tudelft.simulation.supplychain.process;
 
-import nl.tudelft.simulation.supplychain.dsol.SupplyChainSimulatorInterface;
+import org.djutils.exceptions.Throw;
+
+import nl.tudelft.simulation.supplychain.actor.Role;
 
 /**
  * An AutonomousProcess belong to a role, and carries out actions that do not relate to a received message. Examples are
@@ -10,14 +12,36 @@ import nl.tudelft.simulation.supplychain.dsol.SupplyChainSimulatorInterface;
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @param <R> the role to which this autonomous process belongs
  */
-public interface AutonomousProcess
+public abstract class AutonomousProcess<R extends Role<R>>
 {
+    /** the specific Role (R) to which this process belongs. */
+    private final R role;
+    
+    /**
+     * Create the Autonomous process.
+     * @param role the specific Role (R) to which this process belongs
+     */
+    public AutonomousProcess(final R role)
+    {
+        Throw.whenNull(role, "role cannot be null");
+        this.role = role;
+    }
+    
     /**
      * Initialize the processing at the start of the simulation. This method needs to schedule the autonomous activities that
      * need to take place, one-time or repeatedly.
-     * @param simulator the simulator to schedule the autonomous processes
      */
-    void init(SupplyChainSimulatorInterface simulator);
+    public abstract void init();
+
+    /**
+     * Return the specific Role (R) to which this process belongs.
+     * @return the specific Role (R) to which this process belongs
+     */
+    protected R getRole()
+    {
+        return this.role;
+    }
     
 }
