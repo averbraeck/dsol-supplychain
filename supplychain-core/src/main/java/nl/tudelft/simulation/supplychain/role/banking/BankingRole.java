091ab87c58947b1453a90808f82265d002d4bdae
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-03-31 21:39:23 +0200

    Implement BankingRole with the InterestProcess

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/banking/BankingRole.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/banking/BankingRole.java
@@ -1,8 +1,22 @@
 package nl.tudelft.simulation.supplychain.role.banking;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.djutils.event.EventType;
+import org.djutils.exceptions.Throw;
+import org.djutils.metadata.MetaData;
+import org.djutils.metadata.ObjectDescriptor;
+
+import nl.tudelft.simulation.supplychain.actor.Actor;
 import nl.tudelft.simulation.supplychain.actor.Role;
+import nl.tudelft.simulation.supplychain.content.Content;
 import nl.tudelft.simulation.supplychain.content.receiver.ContentReceiver;
 import nl.tudelft.simulation.supplychain.content.receiver.ContentReceiverDirect;
+import nl.tudelft.simulation.supplychain.finance.Money;
+import nl.tudelft.simulation.supplychain.process.AutonomousProcess;
 
 /**
  * The BankingRole maintains the interest rates for the Bank accounts. In this case, we have chosen to not make the Bank work
@@ -14,7 +28,7 @@ import nl.tudelft.simulation.supplychain.content.receiver.ContentReceiverDirect;
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class BankingRole extends Role
+public class BankingRole extends Role<BankingRole>
 {
     /** */
     private static final long serialVersionUID = 20230413L;
@@ -22,8 +36,22 @@ public class BankingRole extends Role
     /** the interest rate for a positive bank account. */
     private double annualInterestRatePos = 0.025;
 
-    /** the interest rate for a negative bank account. */
-    private double annualInterestRateNeg = 0.08;
+    /** the interest rate for a negative bank account, as a negative number. */
+    private double annualInterestRateNeg = -0.08;
+
+    /** the balance of the actors. */
+    private final Map<Actor, Money> bankAccounts = new LinkedHashMap<>();
+
+    /** for who is interested, the BankAccount can send updates of changes. */
+    public static final EventType BANK_ACCOUNT_CHANGED_EVENT = new EventType("BANK_ACCOUNT_CHANGED_EVENT",
+            new MetaData("account", "bank account", new ObjectDescriptor("balance", "bank balance", double.class)));
+
+    /** the necessary content handlers. */
+    private static Set<Class<? extends Content>> necessaryContentHandlers = Collections.emptySet();
+
+    /** the necessary content handlers. */
+    private static Set<Class<? extends AutonomousProcess<BankingRole>>> necessaryAutonomousProcesses =
+            Set.of(InterestProcess.class);
 
     /**
      * Create a new FinancingRole with an attached BankAccount.
@@ -47,8 +75,74 @@ public class BankingRole extends Role
     }
 
     /**
-     * Return the negative annual interest rate.
-     * @return negative annual interest rate
+     * Return the bank balance.
+     * @param actor the actor for which to request the balance
+     * @return the bank balance
+     */
+    public Money getBalance(final Actor actor)
+    {
+        return this.bankAccounts.get(actor);
+    }
+
+    /**
+     * Add money to the bank balance.
+     * @param actor the actor for which to add money to the bank account
+     * @param amount the amount of money to add
+     */
+    public synchronized void addToBalance(final Actor actor, final Money amount)
+    {
+        Money newBalance = roundBalance(this.bankAccounts.get(actor).plus(amount));
+        this.bankAccounts.put(actor, newBalance);
+        sendBalanceUpdateEvent(actor, newBalance);
+    }
+
+    /**
+     * Withdraw money from the bank balance.
+     * @param actor the actor for which to withdraw money from the bank account
+     * @param amount the amount of money to withdraw
+     */
+    public synchronized void withdrawFromBalance(final Actor actor, final Money amount)
+    {
+        Money newBalance = roundBalance(this.bankAccounts.get(actor).minus(amount));
+        this.bankAccounts.put(actor, newBalance);
+        sendBalanceUpdateEvent(actor, newBalance);
+    }
+
+    /**
+     * Send a BANK_ACCOUNT_CHANGED_EVENT to signal an update of the bank balance.
+     * @param actor the actor whose balance has changed
+     * @param newBalance the new balance of the bank account
+     */
+    protected void sendBalanceUpdateEvent(final Actor actor, final Money newBalance)
+    {
+        this.fireTimedEvent(BANK_ACCOUNT_CHANGED_EVENT, newBalance, getActor().getSimulatorTime());
+    }
+
+    /**
+     * Round the amount of money.
+     * @param money the amount of money to round
+     * @return the rounded amount of money
+     */
+    protected Money roundBalance(final Money money)
+    {
+        return new Money(0.01 * Math.round(100.0 * money.getAmount()), money.getMoneyUnit());
+    }
+
+    @Override
+    protected Set<Class<? extends Content>> getNecessaryContentHandlers()
+    {
+        return necessaryContentHandlers;
+    }
+
+    @Override
+    protected Set<Class<? extends AutonomousProcess<BankingRole>>> getNecessaryAutonomousProcesses()
+    {
+        return necessaryAutonomousProcesses;
+    }
+
+    /**
+     * Return the negative annual interest rate, provided as a negative number.
+     * @return negative annual interest rate, as a negative number
      */
     public double getAnnualInterestRateNeg()
     {
@@ -56,11 +150,12 @@ public class BankingRole extends Role
     }
 
     /**
-     * Set a new negative annual interest rate.
-     * @param annualInterestRateNeg new negative annual interest rate
+     * Set a new negative annual interest rate, provided as a negative number.
+     * @param annualInterestRateNeg new negative annual interest rate, as a negative number
      */
     public void setAnnualInterestRateNeg(final double annualInterestRateNeg)
     {
+        Throw.when(annualInterestRateNeg > 0.0, IllegalArgumentException.class, "negative interest rate should be < 0");
         this.annualInterestRateNeg = annualInterestRateNeg;
     }
 
@@ -79,7 +174,17 @@ public class BankingRole extends Role
      */
     public void setAnnualInterestRatePos(final double annualInterestRatePos)
     {
+        Throw.when(annualInterestRatePos < 0.0, IllegalArgumentException.class, "positive interest rate should be > 0");
         this.annualInterestRatePos = annualInterestRatePos;
+    }
+
+    /**
+     * Return the bank accounts per actor with their balance.
+     * @return the bank accounts per actor with their balance
+     */
+    protected Map<Actor, Money> getBankAccounts()
+    {
+        return this.bankAccounts;
     }
 
 }
---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/banking/InterestProcess.java
@@ -0,0 +1,57 @@
+package nl.tudelft.simulation.supplychain.role.banking;
+
+import org.djunits.unit.DurationUnit;
+import org.djunits.value.vdouble.scalar.Duration;
+
+import nl.tudelft.simulation.supplychain.process.AutonomousProcess;
+
+/**
+ * InterestProcess is an autonomous process to provide interest (positive or negative) on the bank acount balance on a
+ * day-to-day basis, based on the annual interest rates.
+ * <p>
+ * Copyright (c) 2023-2025 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class InterestProcess extends AutonomousProcess<BankingRole>
+{
+
+    /**
+     * Create the autonomous interest process.
+     * @param role the BankingRole to which this process belongs
+     */
+    public InterestProcess(final BankingRole role)
+    {
+        super(role);
+    }
+
+    @Override
+    public void init()
+    {
+        getRole().getSimulator().scheduleEventNow(this, "interest", null);
+    }
+
+    /**
+     * receive or pay interest according to the current rates. Note that the negative interest rate is stored as a negative
+     * number.
+     */
+    protected void interest()
+    {
+        for (var account : getRole().getBankAccounts().entrySet())
+        {
+            if (account.getValue().getAmount() < 0)
+            {
+                getRole().addToBalance(account.getKey(),
+                        account.getValue().multiplyBy(getRole().getAnnualInterestRateNeg() / 365.0));
+            }
+            else
+            {
+                getRole().addToBalance(account.getKey(),
+                        account.getValue().multiplyBy(getRole().getAnnualInterestRatePos() / 365.0));
+            }
+        }
+        getRole().getSimulator().scheduleEventRel(new Duration(1.0, DurationUnit.DAY), this, "interest", null);
+    }
+
+}
