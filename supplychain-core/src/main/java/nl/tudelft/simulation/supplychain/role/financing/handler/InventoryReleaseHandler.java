0bcc544edf7526978b7de54ad4b0a688844d9f62
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-04-13 16:56:35 +0200

    PrepareInvoice replaced by InventoryRelease

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/financing/handler/InventoryReleaseHandler.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/financing/handler/InventoryReleaseHandler.java
@@ -5,19 +5,19 @@ import org.djunits.value.vdouble.scalar.Duration;
 
 import nl.tudelft.simulation.supplychain.content.Invoice;
 import nl.tudelft.simulation.supplychain.content.Order;
-import nl.tudelft.simulation.supplychain.content.PrepareInvoice;
+import nl.tudelft.simulation.supplychain.content.InventoryRelease;
 import nl.tudelft.simulation.supplychain.handler.ContentHandler;
 import nl.tudelft.simulation.supplychain.role.financing.FinancingRole;
 
 /**
- * PrepareInvoiceHandler.java.
+ * InventoryReleaseHandler.java.
  * <p>
  * Copyright (c) 2025-2025 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class PrepareInvoiceHandler extends ContentHandler<PrepareInvoice, FinancingRole>
+public class InventoryReleaseHandler extends ContentHandler<InventoryRelease, FinancingRole>
 {
     /** */
     private static final long serialVersionUID = 1L;
@@ -26,24 +26,24 @@ public class PrepareInvoiceHandler extends ContentHandler<PrepareInvoice, Financ
     private Duration paymentTerms = new Duration(7.0, DurationUnit.DAY);
 
     /**
-     * Construct a new PrepareInvoice handler.
+     * Construct a new InventoryRelease handler.
      * @param owner the role belonging to this handler
      */
-    public PrepareInvoiceHandler(final FinancingRole owner)
+    public InventoryReleaseHandler(final FinancingRole owner)
     {
-        super("PrepareInvoiceHandler", owner, PrepareInvoice.class);
+        super("InventoryReleaseHandler", owner, InventoryRelease.class);
     }
 
     @Override
-    public boolean handleContent(final PrepareInvoice prepareInvoice)
+    public boolean handleContent(final InventoryRelease inventoryRelease)
     {
-        if (!isValidContent(prepareInvoice))
+        if (!isValidContent(inventoryRelease))
         {
             return false;
         }
 
         // make and send an Invoice
-        Order order = prepareInvoice.order();
+        Order order = inventoryRelease.inventoryReleaseRequest().inventoryReservation().inventoryReservationRequest().order();
         var invoice = new Invoice(getRole().getActor(), order.sender(), order, getSimulatorTime().plus(this.paymentTerms));
         sendContent(invoice, getHandlingTime().draw());
         return true;
