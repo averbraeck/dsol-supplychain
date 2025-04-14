3c92ab8fe28b526f706d7d00d4205d8c11e73c5c
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-04-14 22:11:15 +0200

    Add InventoryEntryHandler (still has to be changed)

---
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/warehousing/handler/InventoryEntryHandler.java
@@ -0,0 +1,46 @@
+package nl.tudelft.simulation.supplychain.role.warehousing.handler;
+
+import nl.tudelft.simulation.supplychain.content.InventoryEntry;
+import nl.tudelft.simulation.supplychain.handler.ContentHandler;
+import nl.tudelft.simulation.supplychain.role.warehousing.WarehousingRole;
+
+/**
+ * The InventoryEntryHandler implements the business logic for a warehouse that receives an InventoryEntry. It reserves
+ * inventory for later release.
+ * <p>
+ * Copyright (c) 2003-2025 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
+ * The supply chain Java library uses a BSD-3 style license.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class InventoryEntryHandler extends ContentHandler<InventoryEntry, WarehousingRole>
+{
+    /** the serial version uid. */
+    private static final long serialVersionUID = 20221201L;
+
+    /**
+     * Construct a new InventoryEntry handler.
+     * @param owner the role belonging to this handler
+     */
+    public InventoryEntryHandler(final WarehousingRole owner)
+    {
+        super("InventoryEntryHandler", owner, InventoryEntry.class);
+    }
+
+    @Override
+    public boolean handleContent(final InventoryEntry inventoryEntry)
+    {
+        if (!isValidContent(inventoryEntry))
+        {
+            return false;
+        }
+        // Reserve the inventory, even when it is not (yet) available
+        getRole().getInventory().enterOrderedAmount(inventoryEntry.product(), inventoryEntry.amount(),
+                inventoryEntry.shipment().getTotalCargoValue().divideBy(inventoryEntry.shipment().getAmount()));
+        // TODO: fulfillment
+        // var fulfillment = new Fulfillment(inventoryEntry);
+        // sendContent(fulfillment, getHandlingTime().draw());
+        return true;
+    }
+
+}
