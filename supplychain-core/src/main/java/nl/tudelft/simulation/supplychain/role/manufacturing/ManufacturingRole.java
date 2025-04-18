3a7f23b5041b8c4ffacd3c579159358556e2b1ce
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-04-18 07:57:38 +0200

    ManufacturngRole not abstract for now

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/manufacturing/ManufacturingRole.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/role/manufacturing/ManufacturingRole.java
@@ -11,16 +11,16 @@ import nl.tudelft.simulation.supplychain.content.receiver.ContentReceiverDirect;
 import nl.tudelft.simulation.supplychain.product.Product;
 
 /**
- * The producing role is a role that handles the production of products from parts, based on a Bill of Materials (BOM). Parts
- * are sourced from the Inventory, and ready products are added to teh Inventory (Make-to-Stock, MTS) or shipped directly to
- * customers (Make-to-Order, MTO).
+ * The manufacturing role is a role that handles the production of products from parts, based on a Bill of Materials (BOM).
+ * Parts are sourced from the Inventory, and ready products are added to the Inventory (Make-to-Stock, MTS) or shipped directly
+ * to customers (Make-to-Order, MTO).
  * <p>
  * Copyright (c) 2003-2025 Delft University of Technology, Delft, the Netherlands. All rights reserved. <br>
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public abstract class ManufacturingRole extends Role<ManufacturingRole>
+public class ManufacturingRole extends Role<ManufacturingRole>
 {
     /** */
     private static final long serialVersionUID = 20221206L;
