d3a9640bcf9908a0e02e78a3459710890e99aa89
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-09-20 10:40:42 +0200

    Align Javadoc with gscg-design documentation

--- supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/content/InventoryQuoteRequest.java
+++ supplychain-core/src/main/java/nl/tudelft/simulation/supplychain/content/InventoryQuoteRequest.java
@@ -15,12 +15,12 @@ import nl.tudelft.simulation.supplychain.role.warehousing.WarehousingActor;
  * The supply chain Java library uses a BSD-3 style license.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
- * @param sender the sender of the RFW
- * @param receiver the receiver of the RFQ
+ * @param sender the SellingActor that sends the InventoryQuoteRequest
+ * @param receiver the WarehousingActor that receives the message
  * @param timestamp the absolute time when the message was created
  * @param uniqueId the unique id of the message
  * @param groupingId the id used to group multiple messages, such as the demandId or the orderId
- * @param rfq the RequestForQuote from the purchaser
+ * @param rfq the RequestForQuote from the purchaser, containing information about product, amount and due date
  */
 public record InventoryQuoteRequest(SellingActor sender, WarehousingActor receiver, Time timestamp, long uniqueId,
         long groupingId, RequestForQuote rfq) implements GroupedContent, ProductContent
